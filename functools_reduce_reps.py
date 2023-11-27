87bfb0a4bd49c003c54cd0f9797fab975ca7b137
amurfie2003 <amurfie2003@gmail.com>
2023-11-27 04:40:46 -0500

    feat(functools_reduce): add function stub and test

---
+++ functools_reduce_reps.py
@@ -0,0 +1,13 @@
+#Challenge: Create a function that takes a list of integers and returns the 
+#product of all the elements in the list.
+import functools
+numbers = [1, 2, 3, 4, 5, 6]
+
+def multiply(list_numbers):
+    return 0
+
+
+
+#test
+print(f"[1, 2, 3, 4, 5, 6] = {multiply(numbers)}")
+assert multiply(numbers) == 720
