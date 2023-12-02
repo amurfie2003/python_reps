69a2ac0155ec85a14357a9e5ef49a350d213d4cd
amurfie2003 <amurfie2003@gmail.com>
2023-12-02 05:17:08 -0500

    feat(recursion.py): add challenge definition

---
+++ recursion.py
@@ -0,0 +1,4 @@
+#Challenge: Write a recursive function that calculates the nth number in the 
+#Fibonacci sequence. The Fibonacci sequence starts with 0 and 1, and each 
+#subsequent number is the sum of the two preceding ones.
+
